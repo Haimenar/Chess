45bee624fe57f33db6f96c85b321d2a83dd84fec
A M <alec.s.forston@gmail.com>
2024-07-01 22:31:58 -0600

    Fully implemented ChessBoard.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -12,6 +12,75 @@ import java.util.Objects;
 public class ChessBoard {
     private final ChessPiece[][] squares = new ChessPiece[8][8];
 
+    public ChessBoard() {
+    }
+
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        squares[position.getRow() - 1][position.getColumn() - 1] = piece;
+    }
+
+    public void removePiece(ChessPosition position) {
+        squares[position.getRow() - 1][position.getColumn() - 1] = null;
+    }
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+
+    public ChessPiece getPiece(ChessPosition position) {
+        return squares[position.getRow() - 1][position.getColumn() - 1];
+    }
+
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
+    public void resetBoard() {
+
+        //Remove all
+        for (int x = 0; x < 8; x++){
+            for (int y = 0; y < 8; y++){
+                this.squares[x][y] = null;
+            }
+        }
+
+        ChessPiece.PieceType[] typeArray = {ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KING, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.ROOK};
+        int x = 1;
+        int y = 1;
+        //White Pieces
+        for (ChessPiece.PieceType type : typeArray){
+            addPiece(new ChessPosition(x, y), new ChessPiece(ChessGame.TeamColor.WHITE, type));
+            y++;
+        }
+        //White Pawns
+        x = 2;
+        for (int i = 1; i < 9; i++){
+            addPiece(new ChessPosition(x, i), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+        //Black Pieces
+        x = 8;
+        y = 1;
+
+        for (ChessPiece.PieceType type : typeArray){
+            addPiece(new ChessPosition(x, y), new ChessPiece(ChessGame.TeamColor.BLACK, type));
+            y++;
+        }
+        //Black Pawns
+        x = 7;
+        for (int i = 1; i < 9; i++){
+            addPiece(new ChessPosition(x, i), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -23,38 +92,5 @@ public class ChessBoard {
     @Override
     public int hashCode() {
         return Arrays.deepHashCode(squares);
-    }
-
-    public ChessBoard() {
-        
-    }
-
-    /**
-     * Adds a chess piece to the chessboard
-     *
-     * @param position where to add the piece to
-     * @param piece    the piece to add
-     */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
-         squares[position.getRow() - 1][position.getColumn() - 1] = piece;
-    }
-
-    /**
-     * Gets a chess piece on the chessboard
-     *
-     * @param position The position to get the piece from
-     * @return Either the piece at the position, or null if no piece is at that
-     * position
-     */
-    public ChessPiece getPiece(ChessPosition position) {
-        return squares[position.getRow() - 1][position.getColumn() - 1];
-    }
-
-    /**
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
-     */
-    public void resetBoard() {
-        throw new RuntimeException("Not implemented");
     }
 }
